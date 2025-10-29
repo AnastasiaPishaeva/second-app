c8f5097f66efa429cb09530281063b1edf5ab46d
Anastasia <n_pishaeva@mail.ru>
2025-10-29 16:56:37 +0300

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        first: resolve(__dirname, 'index.html'),
+        second: resolve(__dirname, 'meme.html'),
+        third: resolve(__dirname, 'book.html'),
+      },
+    },
+  },
 });
